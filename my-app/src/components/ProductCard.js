32fd9e91a5154baabc0c4f7d2a1931f53c3b154f
srujan186 <srujansrinivas5045@gmail.com>
2025-09-09 23:28:18 +0530

    updated components

---
+++ my-app/src/components/ProductCard.js
@@ -0,0 +1,19 @@
+export function ProductCard(){
+  return (
+    <article>
+      <h2>Iphone 15</h2>
+      <img src="images/mac-mini.png"
+        alt = "Iphone 15"
+        width = "200px"
+        height = "120px"
+      />
+      <p>Specification:</p>
+      <ul>
+        <li>A17 Pro chip with 6-core GPU</li>
+        <li>3x or 5x Telephoto camera</li>
+        <li>Up to 29 hours video playback</li>
+      </ul>
+      <button>Buy (From $999)</button>
+    </article>
+  )
+}
